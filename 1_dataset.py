0179254e87f61c8932187f5de5c209ba614dc27e
JEONGMINKIM <87534918+jjeongmin98@users.noreply.github.com>
2022-07-25 08:46:58 +0900

    0721update

--- 1_dataset.py
+++ 1_dataset.py
@@ -1,86 +1,43 @@
 import RPi.GPIO as GPIO
-
 import cv2
-
 import numpy as np
-
 import os
-
 import time 
 
- 
-
 GPIO.setmode(GPIO.BCM)
-
 PIR_PIN = 17 # OUT pin number
-
 GPIO.setup(PIR_PIN, GPIO.IN)
 
- 
-
- 
-
 recognizer = cv2.face.LBPHFaceRecognizer_create()
-
 recognizer.read('trainer/trainer.yml')
-
 cascadePath = "haarcascades/haarcascade_frontalface_default.xml"
-
 faceCascade = cv2.CascadeClassifier(cascadePath);
-
 font = cv2.FONT_HERSHEY_SIMPLEX
-
- 
-
 #iniciate id counter
-
 id = 0
-
 # names related to ids: example ==> loze: id=1,  etc
-
 # 이런식으로 사용자의 이름을 사용자 수만큼 추가해준다.
-
 names = ['None', 'jeongmin', 'ljy', 'chs', 'ksw']     #id =0 이고 테스트할때 입>
-
- 
-
 # Initialize and start realtime video capture
-
 cam = cv2.VideoCapture(0)
-
 cam.set(3, 640) # set video widht
-
 cam.set(4, 480) # set video height
 
- 
-
 # Define min window size to be recognized as a face
-
 minW = 0.1*cam.get(3)
-
 minH = 0.1*cam.get(4)
-
 try:
-
     time.sleep(2)
-
     print("motion search")
-
     while True:
-
         if GPIO.input(PIR_PIN):
-
-           t = time.localtime()
-
+            t = time.localtime()
             ret, img =cam.read()
             img = cv2.flip(img, 1) # 화면 반전 1 or -1
-
             gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    
             faces = faceCascade.detectMultiScale(gray,scaleFactor = 1.2,
                     minNeighbors = 6,
                     minSize = (int(minW), int(minH)),)
-
             for(x,y,w,h) in faces:
                 cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
                 id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
